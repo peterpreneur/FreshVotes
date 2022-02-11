f11f02ed06fbadab025fa6cab079e1f0270a6921
pas8 <peterpreneur@gmail.com>
2022-02-11 17:51:57 -0600

    entity

--- src/main/java/com/peterpreneur/freshvotes/domain/Feature.java
+++ src/main/java/com/peterpreneur/freshvotes/domain/Feature.java
@@ -2,6 +2,7 @@ package com.peterpreneur.freshvotes.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -12,7 +13,7 @@ public class Feature {
 	private String status;
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/peterpreneur/freshvotes/domain/User.java
+++ src/main/java/com/peterpreneur/freshvotes/domain/User.java
@@ -2,6 +2,7 @@ package com.peterpreneur.freshvotes.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,7 +15,7 @@ public class User {
 	private String description;
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
