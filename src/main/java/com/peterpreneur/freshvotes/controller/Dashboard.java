22cda44e25e4dad7b0ae3e290d6b66647ef84b4d
pas8 <peterpreneur@gmail.com>
2022-02-13 01:44:16 -0600

    Creating Our First Controller

---
+++ src/main/java/com/peterpreneur/freshvotes/controller/Dashboard.java
@@ -0,0 +1,14 @@
+package com.peterpreneur.freshvotes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Dashboard {
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String rootView(){
+		return "index";
+	}
+}
